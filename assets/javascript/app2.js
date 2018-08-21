902242d564546c121799c39c2ac27bf3e93af10e
Hassan Ali Khan <hassanalikhan87@gmail.com>
2018-08-20 19:01:59 -0700

    hopefully works

---
+++ assets/javascript/app2.js
@@ -0,0 +1,24 @@
+// $(".option").on("click", function(){
+//     console.log(this.val());
+// });
+
+var result=0;
+// Event binding using a convenience method
+function result(){
+forEach($("input[value='True']").is(":checked") (function(){
+// $(".option" ).click(function( event ) {
+//     console.log(this.value);
+//     if(this.value==="True"){
+//         result++;
+    
+result++;
+}
+
+))};
+
+$(".btn-danger").click(function(){
+    console.log(result);
+});
+
+
+
